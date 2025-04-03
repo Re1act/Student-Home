30b9b0bab1f7ee091d592d5aa51da9047f6a55f1
re1act <muhammadsaadat27@gmail.com>
2025-04-03 15:24:12 -0400

    test

--- student-home/wsgi.py
+++ student-home/wsgi.py
@@ -1,7 +1,8 @@
 from app import app, db
+from waitress import serve
 
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    serve(app, host='0.0.0.0', port=5000)
